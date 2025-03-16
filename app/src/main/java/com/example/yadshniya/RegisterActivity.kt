9c7f320a572464a0c9fcd58c8c8e7588af82827d
noasasson <noasasson18@gmail.com>
2025-03-16 21:02:09 +0200

    need to continue register with firebase

--- app/src/main/java/com/example/yadshniya/RegisterActivity.kt
+++ app/src/main/java/com/example/yadshniya/RegisterActivity.kt
@@ -7,13 +7,20 @@ import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.auth.auth
 
-class RegisterActivity : AppCompatActivity()  {
+
+class RegisterActivity : AppCompatActivity() {
+    private val auth = Firebase.auth
+
     private lateinit var pickProfileImageButton: ImageButton
 
     //Selecting image callback
@@ -27,6 +34,60 @@ class RegisterActivity : AppCompatActivity()  {
         setUi()
     }
 
+    private fun setUi() {
+        val signUpButton = findViewById<Button>(R.id.registerButton)
+        val gotoLoginButton = findViewById<Button>(R.id.gotoLogin)
+        pickProfileImageButton = findViewById(R.id.profilePicButtonSignUpScreen)
+
+        defineImageSelectionCallBack()
+        pickProfileImageButton.setOnClickListener {
+            Log.i("buttonClick", "pick profile pick button in signup screen clicked")
+            openGallery()
+        }
+
+        gotoLoginButton.setOnClickListener {
+            Log.i("buttonClick", "login button in register screen clicked")
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    //Authentication
+    private fun onSignUp() {
+        val usernameInput = findViewById<TextView>(R.id.usernameTextfield)
+        val username = usernameInput.text.toString().trim()
+
+        val passwordInput = findViewById<TextView>(R.id.passwordTextfield)
+        val password = passwordInput.text.toString().trim()
+
+        auth.createUserWithEmailAndPassword(username, password).addOnSuccessListener {
+            val authenticatedUser = it.user!!
+
+            val profileChange = UserProfileChangeRequest.Builder().setPhotoUri(imageURI)
+                .setDisplayName("$firstNameValue $lastNameValue").build()
+
+            authenticatedUser.updateProfile(profileChange)
+
+            UserModel.instance.addUser(
+                User(authenticatedUser.uid, firstNameValue, lastNameValue), imageURI!!
+            ) {
+                Toast.makeText(
+                    this@RegisterActivity, "Register Successful", Toast.LENGTH_SHORT
+                ).show()
+                val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.addOnFailureListener {
+            Toast.makeText(
+                this@RegisterActivity, "Register Failed, " + it.message, Toast.LENGTH_SHORT
+            ).show()
+        }
+
+    }
+
+
+    //Functions that related to image profile upload
     private fun openGallery() {
         val intent = Intent(MediaStore.ACTION_PICK_IMAGES)
         imageSelectionCallBack.launch(intent)
@@ -66,23 +127,5 @@ class RegisterActivity : AppCompatActivity()  {
                     ).show()
                 }
             }
-    }
-
-    private fun setUi (){
-        val signUpButton = findViewById<Button>(R.id.registerButton)
-        val loginButton = findViewById<Button>(R.id.gotoLogin)
-        pickProfileImageButton = findViewById(R.id.profilePicButtonSignUpScreen)
-
-        defineImageSelectionCallBack()
-        pickProfileImageButton.setOnClickListener {
-            Log.i("buttonClick", "pick profile pick button in signup screen clicked")
-            openGallery()
-        }
-
-        loginButton.setOnClickListener {
-            Log.i("buttonClick", "login button in register screen clicked")
-            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
