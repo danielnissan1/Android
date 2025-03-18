8898b4cf83e8baf78139dda6f071444c2afbf9d5
noasasson <noasasson18@gmail.com>
2025-03-18 14:41:38 +0200

    wait

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,6 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation  ("com.google.firebase:firebase-auth:22.1.1")
 }
