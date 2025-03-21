b568c7967a3aba4dc25bd68d7d8f7d215a775059
noasasson <noasasson18@gmail.com>
2025-03-21 22:53:15 +0200

    added

--- app/src/main/java/com/example/yadshniya/Model/FirebaseModel.kt
+++ app/src/main/java/com/example/yadshniya/Model/FirebaseModel.kt
@@ -1,11 +1,13 @@
 package com.example.yadshniya.Model
 
 import android.graphics.Bitmap
+import android.util.Log
 import android.widget.Toast
 import com.example.yadshniya.EmptyCallback
 import com.example.yadshniya.MyApplication
 import com.example.yadshniya.PostsCallback
 import com.google.android.gms.tasks.Task
+import com.google.firebase.appcheck.FirebaseAppCheck
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
